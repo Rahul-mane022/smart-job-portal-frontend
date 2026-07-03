0de90bb2218d020a0f992f25bea03eb7e89d0d4c
Rahul mane <rahulmane9022@gmail.com>
2026-07-03 21:51:50 +0530

    Added /api to backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://smart-job-portal-backend-wz8u.onrender.com", 
+  baseURL: "https://smart-job-portal-backend-wz8u.onrender.com/api", 
 });
 
 api.interceptors.request.use(
